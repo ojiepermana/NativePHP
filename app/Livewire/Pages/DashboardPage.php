a9965def2aa0bc6798d84c8236a7e8e687160cf2
ojiepermana <me@ojiepermana.com>
2026-02-05 10:22:46 +0700

    Rename mac layout to main

--- app/Livewire/Pages/DashboardPage.php
+++ app/Livewire/Pages/DashboardPage.php
@@ -7,7 +7,7 @@ use Livewire\Component;
 
 class DashboardPage extends Component
 {
-    #[Layout('components.layouts.mac')]
+    #[Layout('components.layouts.main')]
     public function render()
     {
         return view('livewire.pages.dashboard-page');
